783f0e52e57bea31a0182be03697f2b82a2760aa
sreejareddy <sreejamilkuri@gmail.com>
2020-02-20 22:24:24 +0530

    first commit

--- CI.java
+++ CI.java
@@ -1,5 +1,13 @@
 package com.vnrvjiet.sreeja.SI_CI;
 
 public class CI {
-
+	
+		public void CompIntrst(double p, double r, double t)
+		
+		{
+		double com;
+		com=p * Math.pow(1.0+r/100.0,t) - p;
+		System.out.println("compound interest is"+com);
+		}
+	
 }
--- SI.java
+++ SI.java
@@ -1,5 +1,34 @@
 package com.vnrvjiet.sreeja.SI_CI;
 
+import java.util.*;
 public class SI {
+	public static void main(String args[])
+	{
+		double p,r,t,sim;
+		Scanner scan=new Scanner(System.in);
+		try
+		{
+		
+		System.out.println("Enter the amount");
+		//read the input values from keyborad
+		p=scan.nextInt();
+		System.out.println("Enter the rate of interest ");
+		//read the rate of interest
+		r=scan.nextInt();
+		System.out.println("Enter the no. of years");
+		//read the no.years from keyborad
+		t=scan.nextInt();
+		sim=(p * t * r)/100;
+		CI obj=new CI();
+		obj.CompIntrst(p,r,t);
+
+		}
+		catch(Exception e)
+		{
+			System.out.println("enter only numbers");
+		}
+		
+		
+	}
 
 }
